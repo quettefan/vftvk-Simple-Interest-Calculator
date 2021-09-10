426364bd6ac6f159e180c53ba482834d24d4de9c
Steve Winters <swinters@cisco.com>
2021-09-10 14:16:47 -0400

    highlighted outpuy numbers

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function compute()
         window.alert("Enter a positive number");
         window.location.href="index.html";
     } else {
-        return document.getElementById("result").innerHTML="If you deposit "+principal+"," +
-        "\<br\>at an interest rate of "+rate+"%\<br\>" +
-        "You will receive an amount of "+interest+",\<br\>" +
-        "in the year "+year+"\<br\>";
+        return document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>," +
+        "\<br\>at an interest rate of <mark>"+rate+"%</mark>,<br\>" +
+        "You will receive an amount of <mark>"+interest+"</mark>,<br\>" +
+        "in the year <mark>"+year+"</mark><br\>";
     }
 }
 
